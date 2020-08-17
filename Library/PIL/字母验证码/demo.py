d600504d3f4f89f48dd827f0afd31f12810427d1
jonescy <jonescyna@gmail.com>
2020-08-17 15:26:56 +0800

    modify

--- Library/PIL/字母验证码/demo.py
+++ Library/PIL/字母验证码/demo.py
@@ -1,7 +1,17 @@
 import tesserocr
 from PIL import Image
 
-str = 'xxxx.jpg'
-image = Image.open(str)
-result = tesserocr.image_to_text(image).strip()
+image_str = 'xxxx.jpg'
+image = Image.open(image_str).convert('L')
+
+threshold = 160
+table = []
+for i in range(256):
+    if i < threshold:
+        table.append(0)
+    else:
+        table.append(1)
+result = tesserocr.image_to_text(image)
 print(result)
+
+
