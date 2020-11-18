39355016923a5b57bce847844cf83bb9bab594dc
钟锡权 <58173674+Jonescy@users.noreply.github.com>
2020-11-18 16:40:16 +0800

    请求头转成key value形式

--- headers_to_dict.py
+++ headers_to_dict.py
@@ -10,10 +10,12 @@ Referer: https://www.op.gg/statistics/champion/
 User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36
 '''
 header = ''
+# 正则
 p = ':| \n'
 pattern = re.compile(p)
 res = pattern.split(headers)
 print(res)
+# 普通方法
 # for i in headers:
 #     if i == ':':
 #         i = "':'"
