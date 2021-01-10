22a79a4f2f46158099686333ed1b05a2fea1f716
jonescy <jonescyna@gmail>
2021-01-11 04:43:25 +0800

    调整目录

--- Library/内置模块/os/11_os.path 模块.py
+++ Library/内置模块/os/11_os.path 模块.py
@@ -8,17 +8,17 @@ print("判断path所对应是否为绝对路径，返回True或False:{}".format(
 i = os.path.isdir("b.txt")
 print("判断path所对应是否为已存在的目录，返回True或False:{}".format(i))
 
-h = os.path.isfile("../../../basis of python/handle file/b.txt")
+h = os.path.isfile("../../../Basic/handle file/b.txt")
 print("判断path所对应是否为已存在的文件，返回True或False:{}".format(h))
 
-g = os.path.exists("../../../basis of python/handle file/b.txt")
+g = os.path.exists("../../../Basic/handle file/b.txt")
 print("判断path对应文件或目录是否存在，返回True或False:{}".format(g))
 
 # 获取文件基本信息
-k = os.path.getsize("../../../basis of python/handle file/b.txt")
+k = os.path.getsize("../../../Basic/handle file/b.txt")
 print("返回path对应文件的大小:{}k".format(k))
 
-a = os.path.abspath("../../../basis of python/handle file/b.txt")
+a = os.path.abspath("../../../Basic/handle file/b.txt")
 print("返回path在当前路径中的绝对路径:{}".format(a))
 
 b = os.path.normpath("/handle file/b.txt")
@@ -36,17 +36,17 @@ print("返回path中最后的文件名称：{}".format(e))
 f = os.path.join("d:/", "b.txt")
 print("组合path与paths，返回一个路径字符串:{}".format(f))
 
-l = time.ctime(os.path.getctime("../../../basis of python/handle file/b.txt"))
+l = time.ctime(os.path.getctime("../../../Basic/handle file/b.txt"))
 print("返回path对应文件或目录上一次访问的时间:{}".format(l))
 
-m = time.ctime(os.path.getatime("../../../basis of python/handle file/b.txt"))
+m = time.ctime(os.path.getatime("../../../Basic/handle file/b.txt"))
 print("返回path对应文件或目录最近一次修改的时间:{}".format(m))
 
-n = time.ctime(os.path.getmtime("../../../basis of python/handle file/b.txt"))
+n = time.ctime(os.path.getmtime("../../../Basic/handle file/b.txt"))
 print("返回path对应文件或目录的创建时间:{}".format(n))
 
 # 对路径的操作
-path = os.path.abspath("../../../basis of python/handle file/b.txt")
+path = os.path.abspath("../../../Basic/handle file/b.txt")
 print(os.path.split(path))
 print(os.path.splitdrive(path))
 
