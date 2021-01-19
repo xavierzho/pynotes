79e67e830385007f4473766d722f0264a8ef9825
jonescy <jonescyna@gmail>
2021-01-20 05:15:24 +0800

    add

---
+++ Library/WindowsAPI/api.py
@@ -0,0 +1,10 @@
+"""
+@Author: Jonescyna@gmail.com
+@Created: 2021/1/16
+"""
+import win32con
+import win32api
+import win32gui
+import win32ui
+
+win32api
---
+++ test.py
@@ -0,0 +1,29 @@
+nums = [-1, 0, 1, 2, -1, -4]
+nums.sort()
+print(nums)
+
+
+def sum_three():
+    n = len(nums)
+
+    res = []
+    r = n - 1
+    i = 0
+    for a in range(n):
+        l = i + 1
+        for j in range(l, n):
+
+            _sum = [nums[i], nums[l], nums[r]]
+            if sum(_sum) < 0:
+                l += 1
+            elif sum(_sum) == 0:
+                res.append(_sum)
+            else:
+                r -= 1
+            # if l == r:
+            #     i += 1
+            #     break
+    print(res)
+
+
+sum_three()
